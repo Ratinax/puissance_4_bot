00ad95efde3f445fe8883c12d33699722961d9aa
tbernot <tbernot@hackuity.io>
2024-03-14 13:06:47 +0100

    wins on random bot

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import threading
+import io
 grid = [
-	[0,0,0,0,0,0,0],
-	[0,'X',0,0,0,0,0],
-	[0,0,'X',0,0,0,0],
-	[0,0,0,'X',0,0,0],
-	[0,0,0,0,'X',0,0],
-	[0,0,0,0,0,'Y',0],
+	[0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0],
 		]
 # Turns are either 'X' or 'O'
 # pos is between 0 and 6
@@ -14,47 +16,45 @@ def opponent(token):
 		return 'O'
 	return 'X'
 
-def moove_wining(grid, x):
-	def getY(grid, x):
-		y = 0
-		while y < 5 and grid[y][x] == 0:
-			y += 1
-		return y
+def moove_wining(grid, x, y, token):
 	def wins_vertial(grid, x, y):
 		if y > 2:
 			return False
 		for i in range(0, 4):
-			if grid[y + i][x] != grid[y][x]:
+			if grid[y + i][x] != token:
 				return False
 		return True
-	def wins_horizontal(grid, x, y):
-		return 4 * f'{grid[y][x]}' in ''.join(str(token) for token in grid[y])
+	def wins_horizontal(grid, y):
+		return 4 * token in ''.join(str(token) for token in grid[y])
 	def wins_diagonal(grid, x, y):
 		def getResultOfDiagonal(grid, x, y, xGrowing, yGrowing):
 			yi = y
 			xi = x
 			diagonal = ""
-			while yi != 5 * yGrowing != -1 and xi != 6 * xGrowing != -1:
+			yLimit = 5 * (yGrowing != -1)
+			xLimit = 6 * (xGrowing != -1)
+			while yi != yLimit and xi != xLimit:
 				yi += yGrowing
 				xi += xGrowing
 			yGrowing = -yGrowing
 			xGrowing = -xGrowing
-			while yi != 5 * yGrowing != -1 and xi != 6 * xGrowing != -1:
+			yLimit = 5 * (yGrowing != -1)
+			xLimit = 6 * (xGrowing != -1)
+			while yi != yLimit and xi != xLimit:
 				diagonal += str(grid[yi][xi])
 				yi += yGrowing
 				xi += xGrowing
 			if 4 * f'{grid[y][x]}' in diagonal:
 				return True
 			return False
-		return getResultOfDiagonal(grid, x, y, +1, +1) \
-			+ getResultOfDiagonal(grid, x, y, +1, -1) \
-			+ getResultOfDiagonal(grid, x, y, -1, +1) \
-			+ getResultOfDiagonal(grid, x, y, -1, -1) \
+		if getResultOfDiagonal(grid, x, y, +1, +1) : return True
+		if getResultOfDiagonal(grid, x, y, +1, -1) : return True
+		if getResultOfDiagonal(grid, x, y, -1, +1) : return True
+		if getResultOfDiagonal(grid, x, y, -1, -1) : return True
 
-	y = getY(grid, x)
 	if wins_vertial(grid, x, y):
 		return True
-	if wins_horizontal(grid, x, y):
+	if wins_horizontal(grid, y):
 		return True
 	if wins_diagonal(grid, x, y):
 		return True
@@ -63,21 +63,49 @@ def moove_wining(grid, x):
 def can_play_here(grid, x):
 	return grid[0][x] == 0
 
-print(moove_wining(grid, 2))
+def play(grid, token, x):
+	i = 0
+	while i < len(grid) and grid[i][x] == 0:
+		i += 1
+	i -= 1
+	grid[i][x] = token
+	return i
 
-# def find_best_moove(grid, turn = 'X') -> int:
-# 	return
+def copy_gird(grid):
+	grid2 = []
+	for line in grid:
+		grid2.append(line.copy())
+	return grid2
 
-def efficient(grid, token = 'X', max_depth = 4, depth = 0, isMyTurn = True, x = 0):
+def efficient(grid, token = 'X', max_depth = 7, depth = 0, isMyTurn = True, x = 0):
 	if depth == max_depth:
 		return 0
-	# TODO play
-	# If win
-		# IF mon tour
-			# TODO return 7**(max_depth - depth)
-		# ELSE
-			# TODO return - 7**(max_depth - depth)
+	if not can_play_here(grid, x):
+		return 0
+
+	grid_copy = copy_gird(grid)
+	y = play(grid_copy, token, x)
+	if moove_wining(grid_copy, x, y, token):
+		if isMyTurn:
+			return 7**(max_depth - depth)
+		else:
+			return -7**(max_depth - depth)
 	res = 0
 	for i in range(7):
-		res += efficient(grid, otherToken, max_depth, depth + 1, not isMyTurn, i)
-	return res
+		res += efficient(grid_copy, opponent(token), max_depth, depth + 1, not isMyTurn, i)
+	if depth == 0:
+		results[x] = res
+		pass
+	else:
+		return res
+
+results = [0, 0, 0, 0, 0, 0, 0]
+threads = []
+for i in range(7):
+	thread = threading.Thread(target=efficient, args=(grid.copy(), 'O', 6, 0, True, i))
+	threads.append(thread)
+	thread.start()
+	# results.append(efficient(grid, token = 'O', x = i))
+for thread in threads:
+	thread.join()
+print(results)
